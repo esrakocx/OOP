707a7b76dc4ae8b1a3c4cd9cd86593b27a0ca606
Esra Koc <eocesra@gmail.com>
2022-09-20 23:58:17 +0300

    Student information program

---
+++ Student Program/Student Program/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Student's ID: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Student's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Student's surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Student's Midterm 1: ");
+            double midterm1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Student's Midterm 2: ");
+            double midterm2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Student's Final exam: ");
+            double final = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Student's school name: ");
+            string schoolName = Console.ReadLine();
+
+            Student student1 = new Student(id, name, surname, midterm1, midterm2, final, schoolName);
+
+            bool control = true;
+           
+            while(control)
+            {
+                showOperations();
+                Console.Write("Please enter your choice: ");
+                char choice = Convert.ToChar(Console.ReadLine());
+
+                if (choice == '1') 
+                {
+                    student1.printInfo(id, name, surname, midterm1, midterm2, final, schoolName);
+                    Console.WriteLine("\n");
+                    continue;
+                }
+
+                else if (choice == '2')
+                {
+                    double note = student1.calculateGANO(id, midterm1, midterm2, final);
+                    Console.WriteLine("ID - {0} Note - {1}", id, note);
+                    Console.WriteLine("\n");
+                    continue;
+                }
+
+                else if (choice == '3')
+                {
+                    student1.showSchool(id);
+                    Console.WriteLine("\n");
+                    continue;
+                }
+
+                else if (choice == '4')
+                {
+                    control = false;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Incorrect input!");
+                    continue;
+                }
+            }
+            Console.ReadKey();
+        }
+        static void showOperations() 
+        {
+            Console.WriteLine("\n~MENU~\n");
+            Console.WriteLine("1- Show student's info\n2- Show student's GANO\n3- Show student's school\n4- Exit\n");
+        }
+    }
+}
---
+++ Student Program/Student Program/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Program
+{
+    public class Student
+    {
+        private int studentID;
+        private string name;
+        private string surname;
+        private double midterm1;
+        private double midterm2;
+        private double final;
+        private string schoolName;
+
+        public Student(int _studentID, string _name, string _surname, double _midterm1, double _midterm2, double _final, string _schoolName)
+        {
+            studentID = _studentID;
+            name = _name;
+            surname = _surname;
+            midterm1 = _midterm1;
+            midterm2 = _midterm2;
+            final = _final;
+            schoolName = _schoolName;
+        }
+
+        public void printInfo(int studentID, string name, string surname, double midterm1, double midterm2, double final, string schoolName)
+        {
+            Console.WriteLine("Student's ID: {0}", studentID);
+            Console.WriteLine("Student's name: {0}", name);
+            Console.WriteLine("Student's surname: {0}", surname);
+            Console.WriteLine("Student's midterm 1: {0}", midterm1);
+            Console.WriteLine("Student's midterm 2: {0}", midterm2);
+            Console.WriteLine("Student's final: {0}", final);
+        }
+
+        public double calculateGANO(int studentID, double midterm1, double midterm2, double final)
+        {
+            double gano = midterm1 * 0.2 + midterm2 * 0.2 + final * 0.6;
+            return gano;
+        }
+
+        public void showSchool(int studentID)
+        {
+            
+            Console.WriteLine("ID {0} - School Name: {1}", studentID, schoolName);
+        }
+    }
+}
