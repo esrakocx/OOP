e90fe2835dcad2c6565372208e4e9eb1489abecc
Esra Koc <eocesra@gmail.com>
2022-09-21 22:20:24 +0300

    Encapsulation

---
+++ Encapsulation/Encapsulation/Personal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Personal
+    {
+        private string id;
+        public string ID
+        {
+            get
+            {
+                return id.Substring(0, 5) + "******";        
+            }
+
+            set
+            {
+                if (value.Length == 11)
+                {
+                    for(int i=0; i<value.Length; i++)
+                    {
+                        if (char.IsNumber(value[i]))  // IsNumber() returns a bool value
+                        {
+                            id = value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your id has invalid characters!");
+                            break;
+                        }     
+                    }
+                    
+                }
+                else
+                    Console.WriteLine("Your ID is not 11 digits!");   
+            }
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal p1 = new Personal();
+            p1.ID = "12345678910";
+
+            Console.WriteLine("Your ID: " + p1.ID); 
+            
+            Console.ReadKey();
+        }
+
+       
+    }
+}
