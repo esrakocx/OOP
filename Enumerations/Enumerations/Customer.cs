4f68f377dab0913fdfc839ee05f4dc66815df42a
Esra Koc <eocesra@gmail.com>
2022-09-24 21:42:43 +0300

    Intro to enumerations

---
+++ Enumerations/Enumerations/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+
+        public Customer(int id, string name, string surname, string gender, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+            this.gender = gender;
+            this.email = email;
+        }
+
+        public static ArrayList customers = new ArrayList();
+
+        public Status addCustomer(Customer c1)
+        {
+            customers.Add(c1);
+
+            return Status.successful;
+        }
+    }
+}
---
+++ Enumerations/Enumerations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(1, "Esra", "Koc", "Female", "eocesra@gmail.com");
+
+            Status code = customer1.addCustomer(customer1);
+
+            if (code == Status.successful)
+                Console.WriteLine("Customer added successfuly! ");
+            else if (code == Status.unsuccessful)
+                Console.WriteLine("Customer could not be added!");
+            else
+                Console.WriteLine("Connection error!");
+
+            Console.ReadKey();
+        }
+    }
+}
